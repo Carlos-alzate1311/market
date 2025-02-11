61d2f4dce22df13020b612b60cbc3274de0be318
carlos-2022 <carlos.alzateg131122@gmail.com>
2025-02-11 11:40:59 -0500

    Implement getByCategoria and getEscasos methods in ProductoRepository

--- src/main/java/com/project/market/persistence/ProductoRepository.java
+++ src/main/java/com/project/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.project.market.persistence.crud.ProductoCrudRepository;
 import com.project.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -12,4 +13,15 @@ public class ProductoRepository {
     public List<Producto> getAll() {
         return (List<Producto>) (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrdenByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.finByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+
 }
+
