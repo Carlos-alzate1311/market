3d9ec212fbbc86cb4d49383172b1c3b1d46c3973
carlos-2022 <carlos.alzateg131122@gmail.com>
2025-02-24 10:06:23 -0500

    Add ProductService and implement methods in the service layer for product management

--- src/main/java/com/project/market/persistence/ProductoRepository.java
+++ src/main/java/com/project/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.project.market.domain.repository.ProductRepository;
 import com.project.market.persistence.crud.ProductoCrudRepository;
 import com.project.market.persistence.entity.Producto;
 import com.project.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -14,7 +15,10 @@ import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
