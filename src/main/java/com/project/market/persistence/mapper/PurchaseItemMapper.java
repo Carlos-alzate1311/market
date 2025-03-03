81b2dd577a128e3f6b147fa3c8cec52102c48116
carlos-2022 <carlos.alzateg131122@gmail.com>
2025-03-03 11:55:44 -0500

    Add PurchaseMapper to map between Purchase and Compra

--- src/main/java/com/project/market/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/project/market/persistence/mapper/PurchaseItemMapper.java
@@ -8,21 +8,22 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", uses = {ProductMapper.class})
-public interface PurcheseItemMapper {
+public interface PurchaseItemMapper {
 
     @Mappings({
-            @Mapping(source = "id.idProducto",target = "productId"),
-            @Mapping(source = "cantidad",target = "quantity"),
-            @Mapping(source = "estado",target = "active")
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "total", target = "total"),
+            @Mapping(source = "estado", target = "active")
     })
     PurchaseItem toPurchaseItem(ComprasProducto producto);
 
     @InheritInverseConfiguration
     @Mappings({
-            @Mapping(target = "compra",ignore = true),
-            @Mapping(target = "producto",ignore = true),
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
             @Mapping(target = "id.idCompra", ignore = true)
     })
-    ComprasProducto toComprasProducto(PurchaseItem item);
+    ComprasProducto toComprasProducto(PurchaseItem purchaseItem);
 }
 
